fc0fb8858d2530e4a0b6e5a709345bc1a1ce8b16
Keith Irwin <mail@keithirwin.us>
2017-03-29 03:45:56 -0400

    Restored lost edits

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 window.addEventListener("deviceorientation", setRose, true);
+const metric = !(window.navigator.language=='en-US'||window.navigator.language=='my');
+var elevator;
+function gmapsCB(){
+	elevator = new google.maps.ElevationService;
+}
+
 
 if (!navigator.geolocation){ $('#no-gps').show(); }
 else {
 	navigator.geolocation.watchPosition(
 		// success
 		function(pos) {
-			var lat = pos.coords.latitude.toFixed(5);
-			var lon = pos.coords.longitude.toFixed(5);
+			setAltitude(pos.coords.latitude, pos.coords.longitude);
+			var lat = pos.coords.latitude.toFixed(4);
+			var lon = pos.coords.longitude.toFixed(4);
 			lat = (lat.substring(0,1)=='-')? lat.substring(1)+' S' : lat+' N';
 			lon = (lon.substring(0,1)=='-')? lon.substring(1)+' E' : lon+' W';
 			$('#lat').text(lat);
@@ -15,6 +22,7 @@ else {
 		// error
 		function() {
 			$('.coord').hide();
+			$('#rotated').hide();
 			$('#no-gps').show();
 		}, 
 		// options
@@ -22,21 +30,44 @@ else {
 	);
 }
 
+function setAltitude(lat,lon) {
+	if (typeof elevator != 'undefined') {
+		elevator.getElevationForLocations({
+			'locations': [ new google.maps.LatLng(lat,lon) ]
+		}, function(res) {
+			console.log(res);
+			if (res[0].elevation){
+				$('#no-alt').hide();
+				var alt = res[0].elevation;
+				// Convert
+				if (metric) {
+					alt = alt.toFixed(1)+' m';
+				} else {
+					alt = (alt*0.3048).toFixed(1)+' ft';
+				}
+				// Set element text
+				$('#alt').text(alt);
+			} else {
+				// Show error
+				if (res.status && res.status!='OK') {
+					$('#no-alt').text('No altitude data available: '+res.status+'. ');
+				}
+				$('#no-alt').show();
+			}
+		});
+	}
+}
+
 function setRose(e) {
-	
-	// No orientation data
-	if(!e.absolute) {
+	if(!e.absolute) { // No orientation data
+		$('#rotated').hide();
 		$('#no-dir').show();
 	} else {
-		
-		// Rotate rose
 		var rot = 'rotate('+e.alpha.toString().substring(0,5)+'deg)';
 		$('#rose').css({
 			'-ms-transform': rot,
 			'-webkit-transform': rot,
 			'transform': rot
 		});
-
 	}
-	
 }
